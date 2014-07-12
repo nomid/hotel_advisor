c332c5bf9ab7db9188312d9eaf81b720fa6bfb33
Dmitriy Savranskiy <nomid21@gmail.com>
2014-07-12 19:38:34 +0400

    get_rating method, quantity of comments

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,18 @@
 class Hotel < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+
+	def get_rating
+		comments = self.comments
+		if (comments.count == 0)
+			return 'not rated'
+		end
+		summ = 0
+		n = 0
+		comments.each do |c|
+			summ += c.rate
+			n += 1
+		end
+		summ.to_f / n
+	end
 end
